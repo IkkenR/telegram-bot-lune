23c46b4e6c351569b7d2259169e84e0ce05458a8
IkkenR <louisvieille.ai@gmail.com>
2025-04-14 18:28:17 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+import os
+
+TOKEN = os.getenv("7964345406:AAEcOeDZWSDq_OE1uagKm1Dls7CuO3tIwl4")
+MOT_SECRET = "lunevibrante"
+REPONSE_SECRETE = "✨ Bravo ! Le chiffre est : 7 🌙"
+
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message and update.message.text.lower() == MOT_SECRET.lower():
+        await update.message.reply_text(REPONSE_SECRETE)
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+app.run_polling()
